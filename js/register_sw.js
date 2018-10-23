e14d8118f7cdef4438dab38b73cd2dbadb6622d3
gowrieswaran <38650699+gowrieswaran@users.noreply.github.com>
2018-10-23 22:43:08 +0530

    Update register_sw.js

--- js/register_sw.js
+++ js/register_sw.js
@@ -3,7 +3,7 @@
  **/
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/sw.js')
+        .register('./sw.js')
         .then(resp => console.log(`Service Worker Registered!`))
         .catch(err => console.log(`Service Worker Failed! - ${err}`))
 }
